115314679906719bd0f3a720fd18600a88a274a5
dragoono <niklas.grieger1@gmail.com>
2017-09-30 21:07:38 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { MoreComponent } from './more/more.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    AboutComponent,
+    MoreComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
